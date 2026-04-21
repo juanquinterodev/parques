2b316b230c6cd8196a6f2d34141994eb0074399f
juanquinterodev <51691092+juanquinterodev@users.noreply.github.com>
2026-04-21 17:07:55 -0500

    segundo intento

---
+++ JardinBotanico.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class JardinBotanico implements EspacioCultural {
+
+    @Override
+    public void mostrarHorario() {
+        System.out.println("Lugar: Jardín Botánico");
+        System.out.println("Horario: 9:00 AM - 4:30 PM");
+    }
+
+    @Override
+    public String obtenerRequisitoEntrada() {
+        return "Entrada libre (algunos eventos requieren registro)";
+     }
+
+    @Override
+    public void realizarActividadPrincipal() {
+        System.out.println("Actividad: Caminando por el bosque tropical...");
+    }
+
+}
